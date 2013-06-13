e8d4a79fef0acfe3784ee57c417b4d0fdc28f128
Ersin Güvenç <eguvenc@gmail.com>
2013-06-13 12:42:44 +0300

    updated todo

--- modules/welcome/models/todo.php
+++ modules/welcome/models/todo.php
@@ -4,22 +4,21 @@
  * Obullo ODM
  */
 Class UserSchema {
-    function __construct(){
-        $this->config['database'] = 'db';
-        $this->config['table'] = 'users';
-        $this->config['primary_key'] = '_id';
-        
-        $this->user_id = array(
-        'label' => 'ID',
-        'type' => 'int',
-        'rules' => 'trim|integer'
-        );
-        $this->user_password = array(
-        'label' => 'ID',
-        'type' => 'int',
-        'rules' => 'trim|integer'
-        );
-    }
+    public $config = array(
+    'database' => 'db',
+    'table' => 'users',
+    'primary_key' => '_id'
+    );
+    public $user_id = array(
+    'label' => 'ID',
+    'type' => 'int',
+    'rules' => 'trim|integer'
+    );
+    public $user_password = array(
+    'label' => 'ID',
+    'type' => 'int',
+    'rules' => 'trim|integer'
+    );
 }
 
 $user_schema = new \UserSchema();
