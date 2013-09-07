ba590347af4f053ec44ff01595129bd49a0c25f8
eguvenc <eguvenc@gmail.com>
2013-09-07 21:50:54 +0300

    updated modules

--- modules/models/user.php
+++ modules/models/user.php
@@ -10,11 +10,5 @@ Class User extends \Odm {
     
 }
 
-/*
-$user = new Models/User();
-$user->schema->user_id['rules'] = 'trim|integer';
-$user->save();
-*/
-
 /* End of file user.php */
 /* Location: .modules/models/user.php */
