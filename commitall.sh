ee08f083e0be1b0f85db8255ca7759588b030f0c
eguvenc <eguvenc@gmail.com>
2013-09-19 19:26:10 +0300

    updated packages

---
+++ commitall.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+ssh-agent;
+ssh-add;
+
+echo 'Commiting all packages !!';
+
+for file in packages/*; do
+	arr=$(echo $file | tr "/" "\n")
+	for x in $arr
+	do
+		if [ $x != 'packages' ]; then
+			cd /var/www/obullo-2.0/packages/$x
+			git commit -am 'updated package.json'
+			git push origin master
+		fi;
+	done
+done
+
+# cd /var/www/obullo-2.0/packages/$1
+# git commit -am 'updated package.json'
+# git push origin master
