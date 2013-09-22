3c9bcf803614ac4cfcd7497d87d65d9cf8eb9566
eguvenc <eguvenc@gmail.com>
2013-09-22 21:18:33 +0300

    added new package hooks

---
+++ app/config/hooks.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+| -------------------------------------------------------------------------
+| Hooks
+| -------------------------------------------------------------------------
+| This file lets you define "hooks" to extend Framework without hacking the core
+| files.  Please see the user guide for info:
+|
+|	@see docs/advanced/hooks
+|
+| -------------------------------------------------------------------
+| Prototype
+| -------------------------------------------------------------------
+|
+| 	$hooks['pre_controller'] = array(
+|                                'class'    => 'MyClass',
+|                                'function' => 'Myfunction',
+|                                'filename' => 'Myclass.php',
+|                                'filepath' => 'hooks',
+|                                'params'   => array('beer', 'wine', 'snacks')
+|                                );
+|
+*/
+
+$hooks = array();
+
+/* End of file hooks.php */
+/* Location: ./app/config/hooks.php */
