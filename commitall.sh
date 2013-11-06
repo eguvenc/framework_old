11a41053c1ae3faae14c12977cd7e92cc965ce96
eguvenc <eguvenc@gmail.com>
2013-11-06 17:35:05 +0200

    updated package.json

--- commitall.sh
+++ commitall.sh
@@ -4,12 +4,12 @@ ssh-add;
 
 echo 'Commiting all packages !!';
 
-for file in packages/*; do
+for file in modules/packages/*; do
 	arr=$(echo $file | tr "/" "\n")
 	for x in $arr
 	do
 		if [ $x != 'packages' ]; then
-			cd /var/www/framework/packages/$x
+			cd /var/www/framework/modules/packages/$x
 			git commit -am 'updated package.json'
 			git push origin master
 		fi;
