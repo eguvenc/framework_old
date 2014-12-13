a03d1e0c4e72571d77f4c7eb7b75b21502f1e046
eguvenc <eguvenc@gmail.com>
2014-12-13 18:09:03 +0200

    updates

--- public/tutorials/controller/hello_world.php
+++ public/tutorials/controller/hello_world.php
@@ -5,66 +5,41 @@
  * 
  * @var Controller
  */ 
-$app = new Controller(
-    function ($c) {
-        $c->load('view');
 
-        $start = microtime(true);  // start
 
-        // $c->load('service/queue');
+Class Hello_World extends Controller {
 
-        // $this->queue->channel('Log');
-        // $this->queue->push('Workers/Logger', 'Server1.logger', array('log' => array('debug' => 'Test')));
-        // $this->queue->push('Workers/Logger', 'Server1.logger', array('message' => 'This is my message'));
-        // $this->queue->push('Workers/Logger', 'Server1.logger', array('log' => array('debug' => 'Test')));
-
-        // $this->logger->emergency('TEST attempt !', array('username' => 'test2'));
-
-        // // // $this->logger->load('mongo'); 
-        
-        // $this->logger->load('mongo')->filter('priority.notIn', array(LOG_EMERG));
-        // $this->logger->alert('Possible hack attempt !', array('username' => 'test2'));
-        // $this->logger->emergency('MOGO ATTTEMPT !', array('username' => 'test2'));
-        // $this->logger->push();
-
-        // $this->logger->mongo->channel();
-        // $this->logger->mongo->alert();
-
-        // $this->logger->load('email');
-        // $this->logger->email->channel();
-        // $this->logger->email->alert();
-
-        // $this->logger->mongo->channel('test');
-        // $this->logger->mongo->log('alert', '');
-
-        // $a =array();
-        // for ($i=0; $i < 100; $i++) { 
-        //     $a['test__'.$i] = $i;
-        // }
-        // $this->cache->set('Auth:__permanent:Authorized:eguvenc@gmail.com', $a);
-        // $this->cache->hSet('Test:__permanent:Authorized:eguvenc@gmail.com:4454', 'username', 'ersin');
-
-        $end = microtime(true) - $start;  // End Timer
-
-        echo '<br>'.number_format($end, 4);
-
+     /*
+     * [index description]
+     * @return [type] [description]
+     */
+     
+    public function index()
+    {
+        echo 'ok';
     }
-);
+}
 
-$app->func(
-    'index',
-    function () {
+// $app = new Controller(
+//     function ($c) {
+//         $c->load('view');
+//     }
+// );
 
-        $this->view->load(
-            'hello_world',
-            function () {
-                $this->assign('name', 'Obullo');
-                $this->assign('footer', $this->template('footer'));
-            }
-        );
+// $app->func(
+//     'index',
+//     function () {
 
-    }
-);
+//         $this->view->load(
+//             'hello_world',
+//             function () {
+//                 $this->assign('name', 'Obullo');
+//                 $this->assign('footer', $this->template('footer'));
+//             }
+//         );
+
+//     }
+// );
 
 /* End of file hello_world.php */
 /* Location: .public/tutorials/controller/hello_world.php */
