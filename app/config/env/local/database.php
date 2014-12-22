e206e95e201db19c97112b95ce0bbf613165a1bb
artase <ihsancaglayan@gmail.com>
2014-12-22 12:02:22 +0200

    update rbac constants

--- app/config/env/local/database.php
+++ app/config/env/local/database.php
@@ -9,10 +9,11 @@
 return array(
     
     'db' => array(
-        'host' => 'localhost',
-        'username' => env('MYSQL_USERNAME'),
-        'password' => env('MYSQL_PASSWORD'),
-        'database' => 'test',
+        'host'     => '10.0.0.161',
+        'username' => 'root',
+        'password' => '123456',
+        'database' => 'betforyousystem',
+        // 'database' => 'test',
         'port'     => '',
         'charset'  => 'utf8',
         'autoinit' => array('charset' => true, 'bufferedQuery' => true),
