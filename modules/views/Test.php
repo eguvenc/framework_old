a99e545c31d43137a5dc99ff3152d2f9b0cb7927
eguvenc <eguvenc@gmail.com>
2015-02-17 10:49:36 +0200

    updates

--- modules/views/Test.php
+++ modules/views/Test.php
@@ -22,7 +22,7 @@ Class Test extends \Controller
     public function index()
     {
         echo '<pre>Request: <span class="string">'.$this->uri->getUriString().'</span></pre>';
-        echo '<pre>Global Request Object: <span class="string">'.$this->c['request.uri']->getUriString().'</span></pre>';
+        echo '<pre>Global Request Object: <span class="string">'.$this->c['app']->uri->getUriString().'</span></pre>';
         echo '<p>-----------------------------------------</p>';
 
         echo 'VIEWS MODULE:'.$this->router->fetchNamespace().'<br>';
--- modules/welcome/Dummy.php
+++ modules/welcome/Dummy.php
@@ -17,7 +17,7 @@ Class Dummy extends \Controller
     {
         echo '<pre>Request: <span class="string">'.$this->uri->getUriString().'</span></pre>';
         echo '<pre>Response: <span class="string">'.$arg1 .' - '.$arg2. ' - '.$arg3.'</span></pre>';
-        echo '<pre>Global Request Object: <span class="string">'.$this->c['request.uri']->getUriString().'</span></pre>';
+        echo '<pre>Global Request Object: <span class="string">'.$this->c['app']->uri->getUriString().'</span></pre>';
         echo '<p>-----------------------------------------</p>';
 
         echo $this->c['layer']->get('views/test');
