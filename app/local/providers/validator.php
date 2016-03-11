45784a63c0070e346b7db63b26ede03876537628
eguvenc <eguvenc@gmail.com>
2016-03-11 13:09:21 +0200

    added new validator rules

--- app/local/providers/validator.php
+++ app/local/providers/validator.php
@@ -7,9 +7,13 @@ return array(
         'rules' => [
         
             'alpha' => 'Obullo\Validator\Rules\Alpha',
+            'alphaUnicode' => 'Obullo\Validator\Rules\AlphaUnicode',
             'alphadash' => 'Obullo\Validator\Rules\AlphaDash',
+            'alphadashunicode' => 'Obullo\Validator\Rules\AlphaDashUnicode',
             'alnum' => 'Obullo\Validator\Rules\Alnum',
+            'alnumunicode' => 'Obullo\Validator\Rules\AlnumUnicode',
             'alnumdash' => 'Obullo\Validator\Rules\AlnumDash',
+            'alnumdashunicode' => 'Obullo\Validator\Rules\AlnumDashUnicode',
             'csrf' => 'Obullo\Validator\Rules\Csrf',
             'captcha' => 'Obullo\Validator\Rules\Captcha',
             'recaptcha' => 'Obullo\Validator\Rules\ReCaptcha',
@@ -18,7 +22,6 @@ return array(
             'exact' => 'Obullo\Validator\Rules\Exact',
             'iban' => 'Obullo\Validator\Rules\Iban',
             'isbool' => 'Obullo\Validator\Rules\IsBool',
-            'isdecimal' => 'Obullo\Validator\Rules\IsDecimal',
             'isjson' => 'Obullo\Validator\Rules\IsJson',
             'isnumeric' => 'Obullo\Validator\Rules\IsNumeric',
             'matches' => 'Obullo\Validator\Rules\Matches',
